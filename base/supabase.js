dda0ba655f52df40340ab093a92b73b92f845612
Camilo Hernandez <hgcamilo94@gmail.com>
2026-05-15 15:08:32 -0500

    15/05/2026

--- base/supabase.js
+++
@@ -1,7 +0,0 @@
-window.MOTO_CRAZY_SUPABASE = {
-    url: "https://pttwrjcacgxgqezbgpqi.supabase.co",
-    anonKey: "sb_publishable_mDam12TItAwnK4yOpoakJg_U2Gao65g",
-    tables: {
-        accessories: "productos_accesorios"
-    }
-};
